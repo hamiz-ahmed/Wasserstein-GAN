3bb22a1d32f448c8f729eb40a154f5c44246fcd9
Usman Ahmed <ahmedu@tfpool59.informatik.uni-freiburg.de>
2018-02-01 19:21:18 +0100

    config space

---
+++ config_space.py
@@ -0,0 +1,18 @@
+import ConfigSpace as CS
+import logging
+from copy import deepcopy
+logging.basicConfig(level=logging.ERROR)
+
+def create_config_space():
+    config_space = CS.ConfigurationSpace()
+
+    learning_rate = CS.UniformFloatHyperparameter('learning_rate', lower=1e-6, upper=1e-1, default_value=1e-4, log=True)
+    config_space.add_hyperparameter(learning_rate)
+
+    batch_size = CS.UniformIntegerHyperparameter('batch_size', lower=8, default_value=32, upper=512, log=True)
+    config_space.add_hyperparameter(batch_size)
+
+    reg = CS.UniformFloatHyperparameter('reg', lower=1e-8, upper=1e-1, default_value=1e-4, log=True)
+    config_space.add_hyperparameter(reg)
+
+    return config_space
